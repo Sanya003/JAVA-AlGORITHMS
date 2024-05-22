6cd90fd3cf5669d018e9244f47ccf1f4e496d522
Sanya003 <71161092+Sanya003@users.noreply.github.com>
2024-05-22 20:21:50 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/* **********************************************
+ * name: Sanya Ahmed
+ * date: March 21, 2024
+ ************************************************ */
+/** represents a greeting printer */
+public class HelloWorld {
+    /**
+     * print "Hello, World"
+     * 
+     * @param args user inputs - not used
+     */
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
